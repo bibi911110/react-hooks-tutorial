8d9bf9c17b7417356e1c3e1f480a6bcc178c710f
bibo54524 <happidev54524@gmail.com>
2021-11-05 14:10:46 +0900

    changed styling and show amount per produkt

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -20,6 +20,9 @@ const products = [
   }
 ];
 
+function getAmountPerProduct(cart, productName) {
+    return cart.filter(item => item.name === productName).length;
+}
 
 function getTotal(cart) {
   return cart.reduce((totalCost, item) => totalCost + item.price, 0);
@@ -55,19 +58,27 @@ export default function Product() {
 
   return(
     <div className="wrapper">
-      <div>
-        Shopping Cart: {cart.length} total items.
+      <div className="shoppingcart">
+         <b>Shopping Cart</b>
+        <div>
+           {cart.length} total items.
+        </div>
+        <div>Total price: {getTotal(cart)} Euro</div>
       </div>
-      <div>Total: {getTotal(cart)}</div>
-
       <div>
         {products.map(product => (
           <div key={product.name}>
+            <div>
             <div className="product">
               <span role="img" aria-label={product.name}>{product.emoji}</span>
+              
             </div>
-            <button onClick={() => add(product)}>Add</button>
-            <button onClick={() => remove(product)}>Remove</button>
+            
+            </div>
+            <div className="counter">
+              <button onClick={() => add(product)}>+</button>{getAmountPerProduct(cart, product.name)}
+              <button onClick={() => remove(product)}>-</button>
+            </div>
           </div>
         ))}
       </div>
