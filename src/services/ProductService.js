693dfdcef09eddbdead8fbae9e7c4fb49ca60e25
bibo54524 <happidev54524@gmail.com>
2021-11-07 13:13:33 +0900

    changed styling and show amount per produkt

---
+++ src/services/ProductService.js
@@ -0,0 +1,19 @@
+export function fetchProductData() {
+    return [
+      {
+        "emoji": "\uD83C\uDFB8",
+        "name": "gitar",
+        "price": 500
+      },
+      {
+        "emoji": "\uD83C\uDFB7",
+        "name": "saxophone",
+        "price": 1200
+      },
+      {
+        "emoji": "\uD83E\uDD41",
+        "name": "drum",
+        "price": 250
+      }
+    ]
+  }
